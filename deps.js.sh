5cfe8ebd00a0c66ff7e7101f663e1f4b81fe3c44
Mayel de Borniol <git@mayel.space>
2026-08-14 22:57:36 +0100

    deps

--- deps.js.sh
+++ deps.js.sh
@@ -3,7 +3,7 @@
 # Add any extensions/deps with a package.json in their /assets directory here
 # NOTE: any LV Hooks should also be added to ./deps.hooks.js
 
-DEPS='iconify_ex bonfire_ui_common bonfire_editor_milkdown bonfire_geolocate'
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_milkdown bonfire_geolocate bonfire_ui_reactions'
 
 chmod +x ./js-deps-get.sh
 ./js-deps-get.sh "$DEPS" "$@"
